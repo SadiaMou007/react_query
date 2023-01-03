5599366cb04cf47969541632f76379c831217db0
SadiaMou007 <sadia.bu.cse@gmail.com>
2023-01-02 21:40:07 -0800

    react query fetch

--- src/Lessons/1/index.js
+++ src/Lessons/1/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import GeneralFetch from "./GeneralFetch";
-import { ReactQueryFetch } from "./reactQueryFetch";
+import { Random } from "./reactQueryFetch";
 const Lesson1 = () => {
+  const { data, isLoading, isFetching, refetch } = Random();
   return (
-    <>
+    <div className="app">
       <div className="app">
+        <h2>General fetch</h2>
         <GeneralFetch />
       </div>
       <div className="app">
-        {" "}
-        <ReactQueryFetch />
+        <h2>Fetch using react query</h2>
+        <button onClick={() => refetch()}>
+          Click to refetch:
+          {isLoading || isFetching ? "..." : data}
+        </button>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/Lessons/1/reactQueryFetch.js
+++ src/Lessons/1/reactQueryFetch.js
@@ -1,7 +1,20 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+const fetchNumber = () => {
+  return fetch(
+    "https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain&rnd=new"
+  ).then((response) => {
+    if (response.status !== 200) {
+      throw new Error(`Something went wrong. Try again.`);
+    }
 
-const ReactQueryFetch = () => {
-  return <div>reactQueryFetch</div>;
+    return response.json();
+  });
 };
+const Random = () => {
+  const query = useQuery(["random"], fetchNumber);
 
-export { ReactQueryFetch };
+  if (query.isError) return <p>{query.error.message}</p>;
+
+  return query;
+};
+export { Random };
